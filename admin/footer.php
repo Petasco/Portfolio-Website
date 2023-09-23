7eb24224d1d59e3d1f48523572f8fe9f5db2f9a2
DIYOUH PETER <102492516+Petasco@users.noreply.github.com>
2023-09-23 09:11:09 +0000

    Initial commit

---
+++ admin/footer.php
@@ -0,0 +1,69 @@
+
+                                </div>
+                            </div>
+                        </div>
+                    </div> <!-- container -->
+
+                </div> <!-- content -->
+
+                <footer class="footer">
+                    <?=date("Y")?> © Petasco
+                </footer>
+
+            </div>
+
+
+            <!-- ============================================================== -->
+            <!-- End Right content here -->
+            <!-- ============================================================== -->
+
+
+        </div>
+        <!-- END wrapper -->
+
+
+
+        <!-- jQuery  -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/js/metisMenu.min.js"></script>
+        <script src="assets/js/waves.js"></script>
+        <script src="assets/js/jquery.slimscroll.js"></script>
+
+        <!-- Flot chart -->
+        <script src="../plugins/flot-chart/jquery.flot.min.js"></script>
+        <script src="../plugins/flot-chart/jquery.flot.time.js"></script>
+        <script src="../plugins/flot-chart/jquery.flot.tooltip.min.js"></script>
+        <script src="../plugins/flot-chart/jquery.flot.resize.js"></script>
+        <script src="../plugins/flot-chart/jquery.flot.pie.js"></script>
+        <script src="../plugins/flot-chart/jquery.flot.crosshair.js"></script>
+        <script src="../plugins/flot-chart/curvedLines.js"></script>
+        <script src="../plugins/flot-chart/jquery.flot.axislabels.js"></script>
+
+        <!-- KNOB JS -->
+        <!--[if IE]>
+        <script type="text/javascript" src="../plugins/jquery-knob/excanvas.js"></script>
+        <![endif]-->
+        <script src="../plugins/jquery-knob/jquery.knob.js"></script>
+
+        <!-- Dashboard Init -->
+        <script src="assets/pages/jquery.dashboard.init.js"></script>
+
+        <!-- App js -->
+        <script src="assets/js/jquery.core.js"></script>
+        <script src="assets/js/jquery.app.js"></script>
+
+        <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+        <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
+
+        <script>
+            $(document).ready(function() {
+    $('#example').DataTable();
+} );
+        </script>
+
+    </body>
+
+<!-- Mirrored from coderthemes.com/highdmin/vertical/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 09 Apr 2019 06:51:50 GMT -->
+</html>
